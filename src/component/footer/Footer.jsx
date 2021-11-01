6b96330cda2fffc438ba9c2effa77d6b7a31bce0
Arda Nakışçı <anakisci@gmail.com>
2021-11-01 17:14:06 +0300

    fix quick links

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,67 +1,86 @@
 import React, { Component } from "react";
-import {FaTwitter ,FaInstagram ,FaGithub , FaLinkedinIn} from "react-icons/fa";
+import { FaTwitter, FaInstagram, FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const SocialShare = [
-    {Social: <FaGithub /> , link: 'https://github.com/deus-labs'},
-    {Social: <FaTwitter /> , link: 'https://twitter.com/deuslabs'},
-    // {Social: <FaLinkedinIn /> , link: 'https://www.linkedin.com/'},
-    // {Social: <FaInstagram /> , link: 'https://www.instagram.com/'},
-]
+  { Social: <FaGithub />, link: "https://github.com/deus-labs" },
+  { Social: <FaTwitter />, link: "https://twitter.com/deuslabs" },
+  // {Social: <FaLinkedinIn /> , link: 'https://www.linkedin.com/'},
+  // {Social: <FaInstagram /> , link: 'https://www.instagram.com/'},
+];
 
-class Footer extends Component{
-    render(){
-        return(
-            <React.Fragment>
-                <footer className="footer-area">
-                    <div className="footer-wrapper">
-                        <div className="row align-items-end row--0">
-                            <div className="col-lg-12">
-                                <div className="footer-right" data-black-overlay="6">
-                                    <div className="row">
-                                        {/* Start Single Widget  */}
-                                        <div className="col-lg-6 col-sm-6 col-12">
-                                            <div className="footer-link">
-                                                <h4>Quick Link</h4>
-                                                <ul className="ft-link">
-                                                    <li><a href="/portfolio">Work</a></li>
-                                                    <li><a href="/about">About</a></li>
-                                                    <li><a href="/contact">Let's Talk</a></li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                        {/* End Single Widget  */}
-                                        {/* Start Single Widget  */}
-                                        <div className="col-lg-6 col-sm-6 col-12 mt_mobile--30">
-                                            <div className="footer-link">
-                                                <h4>Contact Us</h4>
-                                                <ul className="ft-link">
-                                                    <li><a href="mailto:admin@example.com">info@deuslabs.fi</a></li>
-                                                </ul>
+class Footer extends Component {
+  aboutOnClick = (e) => {
+    e.preventDefault();
+    document.getElementById("about-section").scrollIntoView();
+  };
 
-                                                <div className="social-share-inner">
-                                                    <ul className="social-share social-style--2 d-flex justify-content-start liststyle mt--15">
-                                                        {SocialShare.map((val , i) => (
-                                                            <li key={i}><a href={`${val.link}`}>{val.Social}</a></li>
-                                                        ))}
-                                                    </ul>
-                                                </div> 
-                                            </div>
-                                        </div>
-                                        {/* End Single Widget  */}
+  servicesOnClick = (e) => {
+    e.preventDefault();
+    document.getElementById("services-section").scrollIntoView();
+  };
 
-                                        <div className="col-lg-12">
-                                            <div className="copyright-text">
-                                                <p>Copyright © 2021 deus labs. All Rights Reserved.</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+  render() {
+    return (
+      <React.Fragment>
+        <footer className="footer-area">
+          <div className="footer-wrapper">
+            <div className="row align-items-end row--0">
+              <div className="col-lg-12">
+                <div className="footer-right" data-black-overlay="6">
+                  <div className="row">
+                    {/* Start Single Widget  */}
+                    <div className="col-lg-6 col-sm-6 col-12">
+                      <div className="footer-link">
+                        <h4>Quick Links</h4>
+                        <ul className="ft-link">
+                          <li>
+                            <div onClick={this.aboutOnClick}>About</div>
+                          </li>
+                          <li>
+                            <div onClick={this.servicesOnClick}>Services</div>
+                          </li>
+                        </ul>
+                      </div>
                     </div>
-                </footer>
-            </React.Fragment>
-        )
-    }
+                    {/* End Single Widget  */}
+                    {/* Start Single Widget  */}
+                    <div className="col-lg-6 col-sm-6 col-12 mt_mobile--30">
+                      <div className="footer-link">
+                        <h4>Contact Us</h4>
+                        <ul className="ft-link">
+                          <li>
+                            <a href="mailto:admin@example.com">
+                              info@deuslabs.fi
+                            </a>
+                          </li>
+                        </ul>
+
+                        <div className="social-share-inner">
+                          <ul className="social-share social-style--2 d-flex justify-content-start liststyle mt--15">
+                            {SocialShare.map((val, i) => (
+                              <li key={i}>
+                                <a href={`${val.link}`}>{val.Social}</a>
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                    {/* End Single Widget  */}
+
+                    <div className="col-lg-12">
+                      <div className="copyright-text">
+                        <p>Copyright © 2021 deus labs. All Rights Reserved.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </React.Fragment>
+    );
+  }
 }
 export default Footer;
