bdd6394bd96de44b2b003bcb141557e0eea51241
Arda Nakışçı <anakisci@gmail.com>
2021-11-01 14:51:52 +0300

    add brand logos

--- src/elements/BrandTwo.jsx
+++ src/elements/BrandTwo.jsx
@@ -6,8 +6,23 @@ class BrandTwo extends Component{
             <React.Fragment>
                 <ul className="brand-style-2">
                     <li>
-                        <a href="https://cosmos.network/">
+                        <a href="https://cosmos.network/" target="_blank" rel="noopener noreferrer">
                             <img src="/assets/images/brand/cosmos-logo-white.png" alt="cosmos"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://cosmwasm.com/" target="_blank" rel="noopener noreferrer">
+                            <img src="/assets/images/brand/cosmwasm-logo-white.png" alt="cosmwasm"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://confio.gmbh/" target="_blank" rel="noopener noreferrer">
+                            <img src="/assets/images/brand/confio-logo-white.png" alt="confio"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://junochain.com/" target="_blank" rel="noopener noreferrer">
+                            <img src="/assets/images/brand/juno-logo-white.png" alt="juno"/>
                         </a>
                     </li>
                 </ul>
